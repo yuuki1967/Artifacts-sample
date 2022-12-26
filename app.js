cc3ce895f0b8ed4d9bb9368048a05f918aab15d7
Yuki Endo <11524508+yuuki1967@users.noreply.github.com>
2022-12-26 10:45:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,85 +1,14 @@
-var http = require('http');
-var express = require('express');
-var path = require('path');
-var bodyparser = require('body-parser');
-var mongoose = require('mongoose');
-var mongoSanitize = require('express-mongo-sanitize');
-var Message = require('./schema/Message');
+const http = require('http');
 
-var app = express();
+const hostname = '127.0.0.1';
+const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/chatapp',
-  function(err){
-    if(err){
-      console.error(err);
-    }
-    else{
-      console.log("Successfully connected to MongoDB.");
-    }
-  }
-);
-
-app.use(bodyparser.urlencoded({
-  extended: true
-}));
-
-app.use(bodyparser.json());
-//app.use(mongoSanitize({ allowDots: true, replaceWith: '_'})); //Effect Sanitizer. If it was not specified, NoSQLInjection would occur.
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.get("/", function(req, res, next){
-  Message.find({}, function(err, msgs){
-    if(err) throw err;
-    return res.render('index', {messages: msgs});
-  });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
 
-app.get("/update", function(req, res, next){
-  return res.render('update');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-app.post("/update", function(req, res, next){
-  var newMessage = new Message({
-    username: req.body.username,
-    message: req.body.message
-  });
-  newMessage.save((err)=>{
-    if(err) throw err;
-    return res.redirect("/");
-  });  
-});
-
-app.get("/match", function(req, res, next){
-  return res.render('match');
-});
-
-app.post("/match", function(req, res, next){
-  const usern = req.body.username; //NoSQL injection i.e {"$ne": null}; 
-  query = Message.find({username:usern}, (err, msgs)=>{
-    return res.render('index', {messages: msgs});
-  });
-});
-
-app.get("/search/:name", function(req, res, next){
-  const usern = req.params.name; //NoSQL Injection #63
-  query = Message.find({username:usern});
-  query.select('username message');
-  query.exec((err, msgs)=>{
-	if(err) return HandleError(err); 
-    	return res.render('index', {messages: msgs});
-  });
-});
-
-app.post("/find", function(req, res){
-  var usern = req.body.username; //NoSQL injection i.e {"$ne": null}; 
-  mongoSanitize.sanitize(usern,{ allowDots: true, replaceWith: '_'});
-  query = Message.find({username:usern}, (err, msgs)=>{
-//	  if(err) return HandleError(err); 
-    return res.send({messages: msgs});
-  });
-});
-
-//var server = http.createServer(app);
-app.listen('5000');
