4bb0da00b5a707dfdcf0fd6fe0a0c0e3ce62a4e4
Yuki Endo <11524508+yuuki1967@users.noreply.github.com>
2023-02-03 17:32:22 +0900

    standard fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const http = require('http');
-http.createServer( function(req, res) {
-  res.write('Hello World!');
-  res.end();
-}).listen(8080);
+const http = require('http')
+http.createServer(function (req, res) {
+  res.write('Hello World!')
+  res.end()
+}).listen(8080)
