b0520df8d8e7e1964c00e4ba7493c1dea37de85f
Yuki Endo <11524508+yuuki1967@users.noreply.github.com>
2023-02-03 17:11:16 +0900

    modify app.js

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-function hello(name){
-   console.log("Hello "+name)
-}
-
-function process_request(req, res){
-  hello("Yuki")
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-}
-
-var server = http.createServer(process_request);
-server.listen();
+http.createServer(function(req, res) {
+  res.write('Hello World!')
+  res.end()
+}).listen(8080);
