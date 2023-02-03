4e3cbc0673ec191181d85e4fb14428181533970c
Yuki Endo <11524508+yuuki1967@users.noreply.github.com>
2023-02-03 17:48:31 +0900

    simplest code

--- schema/Message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const Message = mongoose.Schema({
-  username: String,
-  message: String,
-  date: { type: Date, default: new Date() }
-})
-
-module.exports = mongoose.model('Message', Message)
