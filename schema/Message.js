ee2754f9c926344d276f49ccf6f71fd46df8da12
Yuki Endo <11524508+yuuki1967@users.noreply.github.com>
2023-02-03 17:32:36 +0900

    standard fix

--- schema/Message.js
+++ schema/Message.js
@@ -1,9 +1,9 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-var Message = mongoose.Schema({
-    username: String,
-    message: String,
-    date : {type: Date, default: new Date()}
-});
+const Message = mongoose.Schema({
+  username: String,
+  message: String,
+  date: { type: Date, default: new Date() }
+})
 
 module.exports = mongoose.model('Message', Message)
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 describe('#find()', function () {
   it('respond with matching records', function () {
 
-  });
-});
+  })
+})
